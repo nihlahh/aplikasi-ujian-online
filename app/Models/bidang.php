c1cdbf760fe4496b18ac5498d29c8864422951e4
Maulidaa <maulidaallya06@gmail.com>
2025-05-01 23:39:24 +0700

    Refactor Bidang model

--- app/Models/bidang.php
+++ app/Models/bidang.php
@@ -9,16 +9,4 @@ class Bidang extends Model
     protected $connection = 'data_db';
     protected $table = 'm_bidang';
     public $timestamps = false;
-
-    protected $primaryKey = 'kode';
-
-    protected $fillable = [
-        'nama',
-        'type', // Tambahkan type di sini
-    ];
-
-    public function match_soal()
-    {
-        return $this->hasMany(MatchSoal::class, 'bidang_id', 'kode');
-    }
 }
